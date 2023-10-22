5bcc4a2c98ba810eceb3bf9c37f356df4724a31e
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-10-23 01:46:46 +0300

    Add classes for handling Client

---
+++ src/main/java/ru/mirea/database/advice/GlobalExceptionHandlerAdvice.java
@@ -0,0 +1,10 @@
+package ru.mirea.database.advice;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandlerAdvice {
+
+    // TODO: 23.10.2023
+
+}
---
+++ src/main/java/ru/mirea/database/controller/ClientController.java
@@ -0,0 +1,44 @@
+package ru.mirea.database.controller;
+
+import java.util.Optional;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.mirea.database.data.entity.booking.Client;
+import ru.mirea.database.service.ClientService;
+import java.util.NoSuchElementException;
+import java.util.List;
+
+@RestController
+public class ClientController {
+
+    private final ClientService clientService;
+
+    public ClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @PostMapping("/client/register")
+    public ResponseEntity<Client>registerUser(@RequestBody @Valid Client client)
+    {
+        return new ResponseEntity<>(clientService.save(client), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/client")
+    public ResponseEntity<List<Client>>getUsers()
+    {
+        return new ResponseEntity<>(clientService.allClients(), HttpStatus.OK);
+    }
+
+    @GetMapping("/client/{id}")
+    public ResponseEntity<Optional<Client>>getUserById(@PathVariable Long id) throws NoSuchElementException
+    {
+        return new ResponseEntity<>(clientService.getClientById(id), HttpStatus.FOUND);
+    }
+
+}
