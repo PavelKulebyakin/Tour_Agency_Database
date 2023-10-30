9db3eedc8ab29bcdc19d464a08b607144a2d7700
Kulebyakin Pavel <pavel.kulebyakin.study@gmail.com>
2023-10-30 15:54:45 +0300

    Add HousingRepository

--- src/main/java/ru/mirea/database/data/repository/housing/HousingRepository.java
+++ src/main/java/ru/mirea/database/data/repository/housing/HousingRepository.java
@@ -1,8 +1,16 @@
 package ru.mirea.database.data.repository.housing;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import ru.mirea.database.data.entity.housing.Housing;
 
 public interface HousingRepository
-        extends JpaRepository<Housing, Long> {
+        extends JpaRepository<Housing, Long>, JpaSpecificationExecutor<Housing> {
+
+    @Query("SELECT h.name, h.numberOfRooms, h.addres, h.city.name, h.typeOfFood.name FROM Housing as h")
+    Page<Housing> findAll (Specification<Housing> specification, Pageable pageable);
 }
